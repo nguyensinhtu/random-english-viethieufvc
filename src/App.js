5dc0875ce729dff53f3d75412b04fe192b300d9f
tuns <nguyensinhtuit97@gmail.com>
2020-11-01 16:10:54 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import level1 from "./data/level1";
+
+function App() {
+  const [currentIdx, setCurrentIdx] = useState(0);
+
+  const zenNextData = () => {
+    const min = 0;
+    const max = level1.length;
+    let rand = (min + Math.random() * (max - min)) | 0;
+
+    do {
+      rand = (min + Math.random() * (max - min)) | 0;
+    } while (rand == currentIdx);
+
+    setCurrentIdx(rand);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.keyCode == 32) {
+      zenNextData();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keypress", handleKeyPress, false);
+    return () => {
+      document.removeEventListener("keypress", handleKeyPress, false);
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>{currentIdx + 1} - {level1[currentIdx]}</h3>
+        <br />
+        <br />
+        <br />
+        <p style={{ fontSize: 12 }}>Press Space</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
